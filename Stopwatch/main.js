f1ccbd3d1ac4f7a4c0403004cf9d1589772bcfd2
Mary Ojo <38431519+oebunoluwa@users.noreply.github.com>
2020-04-02 18:49:56 +0100

    Add files via upload

---
+++ Stopwatch/main.js
@@ -0,0 +1,84 @@
+// var x = document.getElementById("control");
+
+// x.addEventListener("click", changeIcon);
+// x.addEventListener("click", controlButton);
+
+
+window.onload = function () {
+  
+    var minutes = 00;
+    var seconds = 00; 
+    var tens = 00; 
+    var appendTens = document.getElementById("tens")
+    var appendSeconds = document.getElementById("seconds")
+    var appendMinutes = document.getElementById("minutes")
+    var buttonStart = document.getElementById("play");
+    var buttonStop = document.getElementById("pause");
+    var buttonReset = document.getElementById("reset");
+    var Interval ;
+  
+    buttonStart.onclick = function() {
+      
+       clearInterval(Interval);
+       Interval = setInterval(startTimer, 10);
+    }
+    
+      buttonStop.onclick = function() {
+         clearInterval(Interval);
+    }
+    
+  
+    buttonReset.onclick = function() {
+       clearInterval(Interval);
+      tens = "00";
+        seconds = "00";
+         minutes = "00";
+      appendTens.innerHTML = tens;
+        appendSeconds.innerHTML = seconds;
+        appendMinutes.innerHTML = minutes;
+    }
+    
+     
+    
+    function startTimer () {
+      tens++; 
+      
+      if(tens < 9){
+        appendTens.innerHTML = "0" + tens;
+      }
+      
+      if (tens > 9){
+        appendTens.innerHTML = tens;
+        
+      } 
+
+//seconds
+      
+      if (tens > 99) {
+        console.log("seconds");
+        seconds++;
+        appendSeconds.innerHTML = "0" + seconds;
+        tens = 0;
+        appendTens.innerHTML = "0" + 0;
+      }
+      
+      if (seconds > 9){
+        appendSeconds.innerHTML = seconds;
+      }
+//minutes
+      if (seconds > 59) {
+        console.log("seconds");
+        minutes++;
+        appendMinutes.innerHTML = "0" + minutes;
+        seconds = 0;
+        appendSeconds.innerHTML = "0" + 0;
+      }
+      
+      if (minutes > 9){
+        appendMinutes.innerHTML = minutes;
+      }
+    
+    }
+    
+  
+  }
